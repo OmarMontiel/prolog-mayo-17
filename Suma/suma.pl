fbdcef6571012734442a2efedd76b3b32c7e3754
OmarMontiel <omar.montiel.vazquez@gmail.com>
2017-05-04 09:06:08 -0500

    hola

---
+++ Suma/suma.pl
@@ -0,0 +1,6 @@
+suma(Resultado1,Resultado2,Resultado):-Resultado is Resultado1+Resultado2.
+resta(Resultado1,Resultado2,Resultado):-Resultado is Resultado1-Resultado2.
+
+multiplicacion(Resultado1,Resultado2,Resultado):-Resultado is Resultado1*Resultado2.
+divicion(Resultado1,Resultado2,Resultado):-Resultado is Resultado1/Resultado2.
+
