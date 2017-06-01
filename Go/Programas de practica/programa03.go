52bf99a7de869b3ebf825efec0bd571e444f36b6
OmarMontiel <omar.montiel.vazquez@gmail.com>
2017-06-01 09:10:55 -0500

    Programas de paracticas de go

---
+++ Go/Programas de practica/programa03.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+  "fmt"
+  "math"
+
+)
+
+func main() {
+  fmt.Println("Ahora tines %g problemas",math.Nextafter(2,3))
+}
---
+++ Go/Programas de practica/programas en clase/programa3.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var x, y, z int
+var c, python, java bool
+
+func main() {
+    fmt.Println(x, y, z, c, python, java)
+}
+ var x, y, z int = 1, 2, 3
+var c, python, java = true, false, "¡no!"
